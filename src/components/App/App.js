4fffac8e7f16d6fbffad9f2bbcc2bc154665608c
object-ions <moshikoatia@gmail.com>
2023-12-31 13:37:48 -0800

    Move App to components

---
+++ src/components/App/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Layout from "../Layout/Layout";
+import Home from "../Home/Home";
+import About from "../About/About";
+import Contact from "../Contact/Contact";
+import Projects from "../Projects/Projects";
+import Design from "../Design/Design";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/design" element={<Design />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
