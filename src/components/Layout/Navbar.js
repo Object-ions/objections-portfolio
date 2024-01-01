e633c1f5b5d4db5620525bd874860714b6289f7a
object-ions <moshikoatia@gmail.com>
2023-12-31 20:38:49 -0800

    Add new component

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="Navbar">
+      <ul>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link to={"/projects"}>projects</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="https://github.com/Object-ions" target="blank">
+            Code
+          </a>
+        </li>
+        <li>
+          <Link to={"/design"}>Design</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link to={"/resume"}>Online Resume</Link>{" "}
+        </li>
+        <li>
+          <a href="#">Download Resume</a>{" "}
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
