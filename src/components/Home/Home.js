4d3d5b0f2f0e1c93b86f487b82aa663f5e86e9d0
object-ions <moshikoatia@gmail.com>
2024-01-01 13:51:01 -0800

    Add animation to Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,52 @@
 import React from "react";
 import "./Home.scss";
+import "animate.css";
 
-const HeaderPart = ({ text, id, special }) => (
-  <h2 id={id} className={special ? "special-font" : ""}>
-    {text}
-  </h2>
-);
+const HeaderPart = ({ text, id, special, delay }) => {
+  const style = {
+    animationDelay: `${delay}s`,
+  };
+
+  return (
+    <h2
+      id={id}
+      className={`${
+        special ? "special-font" : ""
+      } animate__animated animate__fadeInDown`}
+      style={style}
+    >
+      {text}
+    </h2>
+  );
+};
 
 const Home = () => {
+  const headerParts = [
+    "from",
+    "the world",
+    "of",
+    "industrial",
+    "DESIGN",
+    "to",
+    "the dynamic",
+    "realm of",
+    "TECHNOLOGY",
+    "my journey blends creativity",
+    "with tech",
+    "expertise.",
+  ];
+
   return (
     <div className="home">
-      <HeaderPart text="from" id="header-1" />
-      <HeaderPart text="the world" id="header-2" />
-      <HeaderPart text="of" id="header-3" />
-      <HeaderPart text="industrial" id="header-4" special />
-      <HeaderPart text="DESIGN" id="header-5" special />
-      <HeaderPart text="to" id="header-6" />
-      <HeaderPart text="the dynamic" id="header-7" />
-      <HeaderPart text="realm of" id="header-8" />
-      <HeaderPart text="TECHNOLOGY" id="header-9" special />
-      <HeaderPart text="my journey blends creativity" id="header-10" />
-      <HeaderPart text="with tech" id="header-11" />
-      <HeaderPart text=" expertise." id="header-12" />
+      {headerParts.map((text, index) => (
+        <HeaderPart
+          key={index}
+          text={text}
+          id={`header-${index + 1}`}
+          special={index === 3 || index === 4 || index === 8}
+          delay={0.3 * index}
+        />
+      ))}
     </div>
   );
 };
