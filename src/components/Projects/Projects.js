3be76d98fcd42aa3b8ea4601b109d05d2647a656
object-ions <moshikoatia@gmail.com>
2024-01-02 10:24:00 -0800

    Add data to code.json. Feed component with data and render the results

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Projects.scss";
+import DataCode from "../../data/code.json";
 
-const Projects = () => {
-  return <div>Projects</div>;
+const ProjectsData = () => {
+  const [projectsData, setProjectsData] = useState([]);
+
+  useEffect(() => {
+    setProjectsData(DataCode);
+    console.log(DataCode);
+  }, []);
+
+  return (
+    <div className="Projects">
+      <div className="item">
+        <h3>{DataCode[0].title}</h3>
+        <p>{DataCode[0].description}</p>
+        <div>
+          {DataCode[0].tags.map((tag, index) => (
+            <span key={index}>{tag}</span>
+          ))}
+        </div>
+        <div className="links">
+          <a href={DataCode[0].github} target="blank" className="github">
+            GitHub Link
+          </a>
+          <a href={DataCode[0].live} target="blank" className="live-site">
+            Live Demo
+          </a>
+        </div>
+        <div className="project-images">
+          {DataCode[0].images && DataCode[0].images > 0 && (
+            <img src={DataCode[0].images[0]} alt={DataCode.title} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Projects;
+export default ProjectsData;
