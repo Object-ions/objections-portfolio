c152d571ca508b2e608d495941e98b9948a4f5a3
object-ions <moshikoatia@gmail.com>
2024-01-02 10:56:31 -0800

    Add image src to 'public/images' and update location in 'code.json' add style to component and format component data with a single fetch

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,24 +14,38 @@ const ProjectsData = () => {
     <div className="Projects">
       <div className="item">
         <h3>{DataCode[0].title}</h3>
+        <hr />
         <p>{DataCode[0].description}</p>
-        <div>
+        <div className="tags">
           {DataCode[0].tags.map((tag, index) => (
-            <span key={index}>{tag}</span>
+            <span key={index} className="tag">
+              {tag}
+            </span>
           ))}
         </div>
         <div className="links">
           <a href={DataCode[0].github} target="blank" className="github">
-            GitHub Link
+            GitHub Repo
           </a>
           <a href={DataCode[0].live} target="blank" className="live-site">
             Live Demo
           </a>
         </div>
-        <div className="project-images">
-          {DataCode[0].images && DataCode[0].images > 0 && (
-            <img src={DataCode[0].images[0]} alt={DataCode.title} />
-          )}
+        <div className="image-wrapper">
+          <a href={DataCode[0].images[0]} target="blank">
+            <img
+              src={DataCode[0].images[0]}
+              alt={DataCode.title}
+              className="single-image"
+            />
+          </a>
+          <a href={DataCode[0].images[0]} target="blank">
+            <img
+              src={DataCode[0].images[0]}
+              alt={DataCode.title}
+              className="single-image"
+            />
+          </a>
         </div>
       </div>
     </div>
