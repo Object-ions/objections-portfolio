c017c9a60291c12397830fa07c9498d95dcf1b0e
object-ions <moshikoatia@gmail.com>
2024-01-02 15:21:12 -0800

    Update the display list of projects title and pass projectID to do display dynamically data side by side

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,53 +1,92 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Projects.scss";
 import DataCode from "../../data/code.json";
 
 const ProjectsData = () => {
-  const [projectsData, setProjectsData] = useState([]);
+  const [selectedProjectId, setSelectedProjectId] = useState(
+    DataCode[0]["item-id"]
+  );
 
-  useEffect(() => {
-    setProjectsData(DataCode);
-    console.log(DataCode);
-  }, []);
+  // Handle clicking on a project title passing its ID
+  const handleProjectClick = (itemId) => {
+    setSelectedProjectId(itemId);
+  };
+
+  // Find the selected project sing project ID
+  const selectedProject = DataCode.find(
+    (project) => project["item-id"] === selectedProjectId
+  );
 
   return (
     <div className="Projects">
-      <div className="item">
-        <h3>{DataCode[0].title}</h3>
-        <hr />
-        <p>{DataCode[0].description}</p>
-        <div className="tags">
-          {DataCode[0].tags.map((tag, index) => (
-            <span key={index} className="tag">
-              {tag}
-            </span>
+      {/* List of Projects */}
+      <div className="project-list">
+        <ul>
+          {DataCode.map((project) => (
+            <li key={project["item-id"]}>
+              <button onClick={() => handleProjectClick(project["item-id"])}>
+                {"> "}
+                {project.title}
+              </button>
+            </li>
           ))}
-        </div>
-        <div className="links">
-          <a href={DataCode[0].github} target="blank" className="github">
-            GitHub Repo
-          </a>
-          <a href={DataCode[0].live} target="blank" className="live-site">
-            Live Demo
-          </a>
-        </div>
-        <div className="image-wrapper">
-          <a href={DataCode[0].images[0]} target="blank">
-            <img
-              src={DataCode[0].images[0]}
-              alt={DataCode.title}
-              className="single-image"
-            />
-          </a>
-          <a href={DataCode[0].images[0]} target="blank">
-            <img
-              src={DataCode[0].images[0]}
-              alt={DataCode.title}
-              className="single-image"
-            />
-          </a>
-        </div>
+        </ul>
       </div>
+
+      {/* Display area */}
+      {selectedProject && (
+        <div className="item">
+          <h3>{selectedProject.title}</h3>
+          <hr />
+          <p>{selectedProject.description}</p>
+
+          {/* Tag list */}
+          <div className="tags">
+            {selectedProject.tags.map((tag, index) => (
+              <span key={index} className="tag">
+                {tag}
+              </span>
+            ))}
+          </div>
+
+          {/* Live Site */}
+          <div className="links">
+            {selectedProject["live-site"] && (
+              <a
+                href={selectedProject["live-site"]}
+                target="blank"
+                className="live-site"
+              >
+                (Live Demo)
+              </a>
+            )}
+
+            {/* Github Repo */}
+            {selectedProject.github && (
+              <a
+                href={selectedProject.github}
+                target="blank"
+                className="github"
+              >
+                (GitHub Repo)
+              </a>
+            )}
+          </div>
+
+          {/* Image wrapper */}
+          <div className="image-wrapper">
+            {selectedProject.images.map((image, imgIndex) => (
+              <a href={image} target="blank" key={imgIndex}>
+                <img
+                  src={image}
+                  alt={`${selectedProject.title}`}
+                  className="single-image"
+                />
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
