d9613af9742f57a8c8dfff146380c5141e011562
object-ions <moshikoatia@gmail.com>
2024-01-03 21:32:59 -0800

    Update Header styles to be responsive

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -37,7 +37,7 @@ const Home = () => {
   ];
 
   return (
-    <div className="home">
+    <div className="Home">
       {headerParts.map((text, index) => (
         <HeaderPart
           key={index}
