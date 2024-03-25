7b78c723095b1df9e48c1f913baffa7a9a9a063d
Object-ions <moshikoatia@gmail.com>
2024-03-24 17:28:15 -0700

    Add new component and styling

--- src/components/Design/Design.js
+++ src/components/Design/Design.js
@@ -1,34 +1,124 @@
-import React from "react";
-import "./Design.scss";
-import "animate.css";
+import React, { useState } from 'react';
+import './Design.scss';
+import DataCode from '../../data/design.json';
+import 'animate.css';
 
-const Design = () => {
-  const animationClass = "animate__animated animate__fadeInUp";
+const ProjectsData = () => {
+  const [selectedProjectId, setSelectedProjectId] = useState(null);
 
-  // Animate the header with 0.1 sec delay
-  const getAnimationDelay = (delaySec) => {
-    const style = { animationDelay: `${delaySec}s` };
-    return { className: animationClass, style };
+  // Handle clicking on a project title passing its ID
+  const handleProjectClick = (itemId) => {
+    setSelectedProjectId(itemId);
   };
 
-  console.log("Design");
+  // Find the selected project sing project ID
+  const selectedProject = DataCode.find(
+    (project) => project['item-id'] === selectedProjectId
+  );
+
+  const fadeInRight = 'animate__animated animate__fadeInRight';
+
   return (
-    <div className="Design">
-      <div className="des-background">
-        <p {...getAnimationDelay(0)}>
-          {"// "}Portfolio Under Construction
-          <hr />
-        </p>
-        <p {...getAnimationDelay(1)}>
-          <br />
-          <span class="color">console.log</span>("Hey there! This part of my
-          portfolio is still being perfected.
-          <br />
-          Can't wait to share it with you. Stay tuned! " );
-        </p>
+    <div className="Projects">
+      {/* List of Projects */}
+      <div className="project-list">
+        <h2 className={fadeInRight}>Designing Projects:</h2>
+        <ul>
+          {DataCode.map((project, index) => (
+            <li
+              key={project['item-id']}
+              className={fadeInRight}
+              style={{ animationDelay: `${index * 0.1}s` }} // Delay each li
+            >
+              <button onClick={() => handleProjectClick(project['item-id'])}>
+                {'> '}
+                {project.title}
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
+
+      {/* Display area */}
+      {selectedProject && (
+        <div className="item" key={selectedProjectId}>
+          <h3 className={fadeInRight} style={{ animationDelay: '0.1s' }}>
+            {selectedProject.title}
+          </h3>
+          <hr className={fadeInRight} style={{ animationDelay: '0.2s' }} />
+          <p className={fadeInRight} style={{ animationDelay: '0.3s' }}>
+            {selectedProject.description}
+          </p>
+
+          {/* Tag list */}
+          <div
+            className={`tags ${fadeInRight}`}
+            style={{ animationDelay: '0.4s' }}
+          >
+            {selectedProject.tags.map((tag, index) => (
+              <span key={index} className="tag">
+                {tag}
+              </span>
+            ))}
+          </div>
+
+          <div
+            className={`links ${fadeInRight}`}
+            style={{ animationDelay: '0.5s' }}
+          >
+            {/* Live Site */}
+            {selectedProject['live'] && (
+              <a
+                href={selectedProject['live']}
+                target="blank"
+                className="live-site"
+              >
+                (Live Demo)
+              </a>
+            )}
+
+            {/* Github Repo */}
+            {selectedProject.github && (
+              <a
+                href={selectedProject.github}
+                target="blank"
+                className="github"
+              >
+                (GitHub Repo)
+              </a>
+            )}
+
+            {/* video */}
+            {selectedProject['videos'] && (
+              <a
+                href={selectedProject['videos']}
+                target="blank"
+                className="live-site"
+              >
+                (Video Demo)
+              </a>
+            )}
+          </div>
+
+          {/* Image wrapper */}
+          <div
+            className={`image-wrapper ${fadeInRight}`}
+            style={{ animationDelay: '0.6s' }}
+          >
+            {selectedProject.images.map((image, imgIndex) => (
+              <a href={image} target="blank" key={imgIndex}>
+                <img
+                  src={image}
+                  alt={`${selectedProject.title}`}
+                  className="single-image"
+                />
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
-export default Design;
+export default ProjectsData;
